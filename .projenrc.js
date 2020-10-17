28c090bf57b86865374ef7d212891f788152e938
Markus Lindqvist <markus.lindqvist@op.fi>
2020-10-17 15:50:26 +0300

    Upgrade to CDK 1.68.0

--- .projenrc.js
+++ .projenrc.js
@@ -3,7 +3,7 @@ const {
   Semver
 } = require('projen');
 
-const AWS_CDK_LATEST_RELEASE = '1.64.0';
+const AWS_CDK_LATEST_RELEASE = '1.68.0';
 const CONSTRUCTS_LATEST_RELEASE = '3.0.4';
 
 const project = new JsiiProject({
