98f33d8f0aee2494443bcf552594b7f3d08364d2
Markus Lindqvist <markus.lindqvist@op.fi>
2020-11-16 15:26:17 +0200

    Upgrade to AWS CDK 1.73.0

--- .projenrc.js
+++ .projenrc.js
@@ -1,9 +1,8 @@
 const {
   AwsCdkConstructLibrary,
-  Semver,
 } = require('projen');
 
-const AWS_CDK_LATEST_RELEASE = '1.68.0';
+const AWS_CDK_LATEST_RELEASE = '1.73.0';
 
 const PROJECT_NAME = 'cdk-codepipeline-bitbucket-build-result-reporter';
 const PROJECT_DESCRIPTION = 'A JSII construct lib for reporting AWS CodePipeline build statuses to a Bitbucket server instance';
@@ -25,18 +24,18 @@ const project = new AwsCdkConstructLibrary({
     '@aws-cdk/aws-lambda',
     '@aws-cdk/aws-lambda-nodejs',
   ],
-  devDependencies: {
-    '@types/aws-lambda': Semver.caret('8.10.61'),
-    '@types/node': Semver.caret('14.6.2'),
-    '@types/node-fetch': Semver.caret('2.5.7'),
-    'parcel': Semver.pinned('2.0.0-beta.1'),
-  },
-  dependencies: {
-    'aws-sdk': Semver.caret('2.734.0'),
-    'aws-lambda': Semver.caret('1.0.6'),
-    'node-fetch': Semver.caret('2.6.1'),
-  },
-  bundledDependencies: [
+  devDeps: [
+    '@types/aws-lambda@^8.10.61',
+    '@types/node@^14.6.2',
+    '@types/node-fetch@^2.5.7',
+    'parcel@2.0.0-beta.1',
+  ],
+  deps: [
+    'aws-sdk@2.734.0',
+    'aws-lambda@^1.0.6',
+    'node-fetch@^2.6.1',
+  ],
+  bundledDeps: [
     'aws-sdk',
     'aws-lambda',
     'node-fetch'
