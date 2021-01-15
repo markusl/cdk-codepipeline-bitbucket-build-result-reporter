da1a27d75aeb32ec9b9b380ba9aef83d56c0d937
Markus Lindqvist <markus.lindqvist@iki.fi>
2021-01-15 19:50:54 +0200

    Update dependencies to latest versions

--- .projenrc.js
+++ .projenrc.js
@@ -2,7 +2,7 @@ const {
   AwsCdkConstructLibrary,
 } = require('projen');
 
-const AWS_CDK_LATEST_RELEASE = '1.84.0';
+const AWS_CDK_LATEST_RELEASE = '1.85.0';
 
 const PROJECT_NAME = 'cdk-codepipeline-bitbucket-build-result-reporter';
 const PROJECT_DESCRIPTION = 'A JSII construct lib for reporting AWS CodePipeline build statuses to a Bitbucket server instance';
@@ -28,7 +28,7 @@ const project = new AwsCdkConstructLibrary({
     '@types/aws-lambda@^8.10.64',
     '@types/node@^14.14.9',
     '@types/node-fetch@^2.5.7',
-    'esbuild@0.8.31',
+    'esbuild@0.8.32',
   ],
   deps: [
     'aws-sdk@2.817.0',
