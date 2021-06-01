443c5aa3591b0d18bc9669b4c343c11e2848f6f2
Markus Lindqvist <markus.lindqvist@iki.fi>
2021-06-01 09:58:00 +0300

    feat: CDK 1.106.1

--- .projenrc.js
+++ .projenrc.js
@@ -2,7 +2,7 @@ const {
   AwsCdkConstructLibrary,
 } = require('projen');
 
-const AWS_CDK_LATEST_RELEASE = '1.104.0';
+const AWS_CDK_LATEST_RELEASE = '1.106.1';
 
 const PROJECT_NAME = 'cdk-codepipeline-bitbucket-build-result-reporter';
 const PROJECT_DESCRIPTION = 'A JSII construct lib for reporting AWS CodePipeline build statuses to a Bitbucket server instance';
@@ -35,10 +35,10 @@ const project = new AwsCdkConstructLibrary({
     '@types/aws-lambda@^8.10.75',
     '@types/node@^14.14.9',
     '@types/node-fetch@^2.5.7',
-    'esbuild@0.12.0',
+    'esbuild@0.12.5',
   ],
   deps: [
-    'aws-sdk@2.833.0',
+    'aws-sdk@2.918.0',
     'aws-lambda@^1.0.6',
     'node-fetch@^2.6.1',
   ],
